625e85c33fad20ac9264e8535434fa9882094071
William Seong <seongwilliam@gmail.com>
2025-02-21 20:00:50 -0500

    Deleted default vue app, replaced with temp home page and about page

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,11 @@
+<style setup lang="ts">
+
+</style>
+
+<template>
+    <h1>Young By Nail About Coming Soon</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,12 @@
+<style setup lang="ts">
+
+</style>
+
+<template>
+    <h1>Young By Nail Coming Soon</h1>
+
+    <router-link to="/about">Router Test</router-link>
+</template>
+
+<style scoped>
+</style>
