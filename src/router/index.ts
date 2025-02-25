5c5dd189adacd89b75c4a49ea403d3c3c46c130f
William Seong <seongwilliam@gmail.com>
2025-02-24 20:24:27 -0500

    Removing Test component for push

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../components/HomePage.vue";
 import Menu from "../components/menu/MainMenu.vue";
-import Test from "../components/menu/TestComponent.vue";
+// import Test from "../components/menu/TestComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/menu', name: 'menu', component: Menu},
-    { path: '/test', name: 'test', component: Test}
+    // { path: '/test', name: 'test', component: Test}
   ],
 })
 
